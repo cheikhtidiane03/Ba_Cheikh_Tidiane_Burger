99ee8c74c5a92f21b0c6e19f9136f0ba6aac6834
Cheikh Tidiane Ba <cheikhtidianeb764@gmail.com>
2026-04-07 12:32:52 +0000

    feat: Ajout d'autres Seeders pour categories

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -11,12 +11,12 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         $categories = [
-            ['name' => 'Burgers Classiques',  'description' => 'Nos incontournables burgers traditionnels'],
-            ['name' => 'Burgers Spéciaux',    'description' => 'Créations exclusives du chef'],
-            ['name' => 'Burgers Végétariens', 'description' => 'Burgers sans viande, 100% savoureux'],
-            ['name' => 'Menus',               'description' => 'Burger + frites + boisson'],
-            ['name' => 'Boissons',            'description' => 'Sodas, jus et boissons fraîches'],
-            ['name' => 'Desserts',            'description' => 'Pour finir en beauté'],
+            ['name' => 'Categorie Burgers ISI1 ',  'description' => 'Description de la catégorie Burgers ISI1'],
+            ['name' => 'Categorie Burgers ISI2',  'description' => 'Description de la catégorie Burgers ISI2'],
+            ['name' => 'Categorie Burgers ISI3',  'description' => 'Description de la catégorie Burgers ISI3'],
+            ['name' => 'Categorie Burgers ISI4',  'description' => 'Description de la catégorie Burgers ISI4'],
+            ['name' => 'Categorie Burgers ISI5',  'description' => 'Description de la catégorie Burgers ISI5'],
+            ['name' => 'Categorie Burgers ISI6',  'description' => 'Description de la catégorie Burgers ISI6'],
         ];
 
         foreach ($categories as $cat) {
@@ -29,6 +29,6 @@ class CategorySeeder extends Seeder
             );
         }
 
-        $this->command->info('✅ Catégories créées : ' . count($categories));
+        $this->command->info('Catégories créées : ' . count($categories));
     }
 }
